6e98ec43ebe813380424de8ebc4c46a5ce98ff3f
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-05-06 15:09:07 -0700

    feat(ds&a): add data structure implementations, array and string leetcode questions

---
+++ algorithms/arrays/exercises/0.5.pairSum.js
@@ -0,0 +1,3 @@
+// given collection, sum determine if there is a matching pair in collection that sums to sum
+// ex: [1, 2, 3, 9], 8 => false
+// ex: [1, 2, 4, 4], 8 => true 
---
+++ algorithms/arrays/exercises/04.mergeSorted.js
@@ -0,0 +1,4 @@
+/**
+ * Given two sorted arrays as inputs, write a function
+ * that returns a merged array in the correct order
+ */
