844a96b00f574302f807ebadb540979335cfd85c
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-05-13 20:27:41 -0700

    feat(recursion): add start and end range in sorted array  question

---
+++ algorithms/recursion/solutions/05.firstAndLastPosition.js
@@ -0,0 +1,38 @@
+/**
+ * Given an array of integers nums sorted in ascending order, 
+ * find the starting and ending position of a given target value.
+ * 
+ * If target is not found in the array, return [-1, -1].
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ * 
+ * Example 1:
+ * -  Input: nums = [5,7,7,8,8,10], target = 8
+ * -  Output: [3,4]
+ */
+
+// Key points
+
+// 1. Verify constraints
+
+// 2. Test cases
+
+// 3. Brute force
+
+// 4. Code the brute force
+
+// 5. Errors
+
+// 6. Test 
+
+// 7. Space and time
+
+// 8. Optimize 
+
+// 9. Code the optimization
+
+// 10. Test, space and time
+
+// 11. Ask the interviewer if okay
